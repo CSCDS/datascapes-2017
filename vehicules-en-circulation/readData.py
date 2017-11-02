71f70833157fa5e3b60862b262364b06752f283c
shrutibhanderi <frooty2308@gmail.com>
2017-11-02 17:51:49 -0400

    not needed anymore

--- vehicules-en-circulation/readData.py
+++
@@ -1,8 +0,0 @@
-#vehicules-circulation-2016 dataset
-#import pandas for reading csv file into dataframe
-import pandas as pd
-
-#read_csv from pandas will read the csv file into dataframe(which is table like format)
-df = pd.read_csv('vehicules-circulation-2016.csv')
-#prints first 5 row of the dataframe
-print(df.head(5))
